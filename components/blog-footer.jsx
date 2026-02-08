95a34bfc2b8d13cc547dec233d6a6aa944133887
Ezra Sowden-Guzman <ezratic@gmail.com>
2026-02-08 15:51:50 -0500

    cleanup

--- components/blog-footer.jsx
+++ components/blog-footer.jsx
@@ -4,7 +4,7 @@ export function BlogFooter() {
         <em>A note about AI usage:</em>
     </h5>
     <em>
-        This article was hand-written by real individuals working on Allegory.js. While we use LLMs to great effect to help ideate, review code, and work through architectural decisions, all blog content is created by humans, and reflects the unique experiences and viewpoints of the authors.__
+        This article was hand-written by real individuals working on Allegory.js. While we use LLMs to great effect to help ideate, review code, and work through architectural decisions, all blog content is created by humans, and reflects the unique experiences and viewpoints of the authors.
     </em>
     </>)
 }
