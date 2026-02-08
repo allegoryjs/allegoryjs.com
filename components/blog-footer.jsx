1edbf52c5978e22904f944515200c0599fb0a30b
Ezra Sowden-Guzman <ezratic@gmail.com>
2026-02-08 15:25:17 -0500

    add first blog post (WIP need to integrate into site)

---
+++ components/blog-footer.jsx
@@ -0,0 +1,10 @@
+export function BlogFooter() {
+    return (<>
+    <h5>
+        <em>A note about AI usage:</em>
+    </h5>
+    <em>
+        This article was hand-written by real individuals working on Allegory.js. While we use LLMs to great effect to help ideate, review code, and work through architectural decisions, all blog content is created by humans, and reflects the unique experiences and viewpoints of the authors.__
+    </em>
+    </>)
+}
